deccc09983a45fcb6e5bebbe3920608e36d35302
meenjoon <zqudwnsz@naver.com>
2023-11-30 23:21:47 +0900

    feat: RickMortyApiClient 생성

---
+++ app/src/main/java/com/mbj/composepaging/data/remote/network/api/rick_morty/RickMortyApiClient.kt
@@ -0,0 +1,15 @@
+package com.mbj.composepaging.data.remote.network.api.rick_morty
+
+import com.mbj.composepaging.data.remote.model.RickMorties
+import com.mbj.composepaging.data.remote.network.adapter.ApiResponse
+import com.mbj.composepaging.util.Constants
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RickMortyApiClient {
+
+    @GET(Constants.END_PONT)
+    suspend fun getAllCharacters(
+        @Query("page") page: Int,
+    ): ApiResponse<RickMorties>
+}
