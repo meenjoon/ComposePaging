016af5225d9b6819c6821a50e6b4078d8a1fda35
meenjoon <zqudwnsz@naver.com>
2023-12-01 01:46:29 +0900

    feat: PagingData를 이용한 UI 구현

--- app/src/main/java/com/mbj/composepaging/ui/MainActivity.kt
+++ app/src/main/java/com/mbj/composepaging/ui/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.mbj.composepaging.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -16,10 +25,22 @@ import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.itemsIndexed
+import coil.compose.rememberImagePainter
+import com.mbj.composepaging.data.remote.model.RickMorty
 import com.mbj.composepaging.ui.theme.ComposePagingTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +51,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    val viewModel: MainViewModel = hiltViewModel()
+                    val pagingRickMorty = viewModel.rickMorty.collectAsLazyPagingItems()
+
                     Greeting(
+                        rickMortyItem = pagingRickMorty,
                         modifier = Modifier.fillMaxSize()
                     )
                 }
@@ -40,7 +65,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
+fun Greeting(rickMortyItem: LazyPagingItems<RickMorty>, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -60,15 +85,64 @@ fun Greeting(modifier: Modifier = Modifier) {
                     modifier = Modifier.align(Alignment.CenterHorizontally),
                     tint = Color.Yellow
                 )
+
+                LazyColumn(
+                    modifier = modifier,
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    itemsIndexed(rickMortyItem) { index, item ->
+                        if (item != null) {
+                            Log.d("PagingTest", "Item at index $index: ${item.name}")
+                            RickMortyItem(rickMortyItem = item)
+                        }
+                    }
+                }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposePagingTheme {
-        Greeting()
+fun RickMortyItem(rickMortyItem: RickMorty) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = 8.dp
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            val painter = rememberImagePainter(
+                data = rickMortyItem.image,
+                builder = {
+                    crossfade(true)
+                }
+            )
+            Image(
+                painter = painter,
+                contentDescription = "Rick and Morty",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .clip(shape = RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = rickMortyItem.name!!,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(text = "성별: ${rickMortyItem.gender}")
+
+        }
     }
 }
